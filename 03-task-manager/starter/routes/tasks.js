14872ba69dea5be616d9bc0e34aea77f05c62c00
mantisdev20 <aloroduns@yahoo.com>
2025-01-23 01:48:29 -0500

    week6

--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -6,9 +6,11 @@ const {
     createTask,
     getTask,
     updateTask,
-    deleteTask
+    deleteTask,
+    editTask,
 } = require('../controllers/tasks');
 
 router.route('/').get(getAllTasks).post(createTask);
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
+router.route('/:id/edit').patch(editTask)
 module.exports = router;
